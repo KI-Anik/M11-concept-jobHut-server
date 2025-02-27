1df164f0c1044d99b54b89873011ec1d6707f221
KI-Anik <iamkhairul101@gmail.com>
2025-02-27 12:20:15 +0600

    add endpoints to retrieve jobs by user email and delete jobs by ID

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
 
 const port = process.env.PORT || 9000
@@ -34,6 +34,21 @@ async function run() {
     // get jobs data from mongoDB
     app.get('/jobs', async (req, res) => {
       const result = await jobsCollection.find().toArray()
+      res.send(result)
+    })
+
+    // get all jobs posted by a specific user
+    app.get('/jobs/:email', async(req,res)=>{
+      const email = req.params.email;
+      const query = {'buyer.email': email}
+      const result = await jobsCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.delete('/job/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query ={_id:new ObjectId(id)}
+      const result = await jobsCollection.deleteOne(query)
       res.send(result)
     })
 
